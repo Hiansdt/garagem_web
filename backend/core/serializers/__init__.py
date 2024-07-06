cab5a2c72afc78249fecba3c1982a56206d5a216
Hiansdt <vnhiansdt@gmail.com>
2024-07-05 22:23:42 -0300

    FEAT: Add migration for altering marca nacionalidade modelo and create Modelo model

--- backend/core/serializers/__init__.py
+++ backend/core/serializers/__init__.py
@@ -3,4 +3,4 @@ from .acessorio import AcessorioSerializer
 from .categoria import CategoriaSerializer
 from .cor import CorSerializer
 from .marca import MarcaSerializer
-from .modelo import ModeloSerializer
+from .modelo import ModeloGetSerializer, ModeloPostSerializer
--- backend/core/serializers/modelo.py
+++ backend/core/serializers/modelo.py
@@ -2,7 +2,13 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Modelo
 
-class ModeloSerializer(ModelSerializer):
+class ModeloGetSerializer(ModelSerializer):
+    class Meta:
+        model = Modelo
+        fields = "__all__"
+        depth = 1
+
+class ModeloPostSerializer(ModelSerializer):
     class Meta:
         model = Modelo
         fields = "__all__"
