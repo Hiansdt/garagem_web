6bac85aef3cb73ef47182c83ffd17c41c9f70d31
Hiansdt <vnhiansdt@gmail.com>
2024-07-05 22:51:43 -0300

    FEAT: Add VeiculoView component and VeiculosApi

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -38,6 +38,11 @@ const router = createRouter({
       component: () => import("../views/CorView.vue"),
     },
     {
+      path: "/veiculos",
+      name: "veiculos",
+      component: () => import("../views/VeiculoView.vue"),
+    },
+    {
       path: "/login",
       name: "login",
       component: LoginView,
